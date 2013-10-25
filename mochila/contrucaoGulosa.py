9432b20393fd8121218e98562b01e498e75dfa37
alexandre <alexandre@alexandre-CTI.(none)>
2013-10-25 14:32:43 -0200

    Adicionado arquivo da contrução gulosa

---
+++ mochila/contrucaoGulosa.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import os, sys
+
+# Contrução Gulosa do Problema da Mochila
+# Autor: Alexandre Gomes da Costa
+# Disciplina: Sistemas Evolutivo 2013/2
+
+
+
+
+def inicializaC():
+	## Peso
+	w = [4,5,7,9,6]
+	## Valor
+	p = [2,2,3,4,4]
+	## Objetos
+	j = [w,p]
+
+	return j
+
+def melhor(lista):
+	for i in len(lista)
+		
+
+def contrucaoGulosa():
+	s = []
+	c = inicializaC()
+
+	for i in range(len(c)):
+		
