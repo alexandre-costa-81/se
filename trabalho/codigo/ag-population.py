7f5e9da86926188f92c5bca695c9dce2cf21bd11
Alexandre Costa <alexandre.gcosta@gmail.com>
2014-01-15 11:07:59 -0200

    adicionando referencias do artigo e fazendo commit inical do codigo

---
+++ trabalho/codigo/ag-population.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created: 15/01/2014
+@autor: Alexandre Costa
+@disciplina: Sistemas Evolutivos
+@professor: Marilton Aguiar
+@descrição: trabalho final da @disciplina, onde foi desenvolvido um Algoritmo Genético que simula o ataque de pragas periódicas a um conjunto de colonias.
+"""
+
+# Packages
+from Tkinter import *
+import random
+import time
+
+# GLOBAL
+
+
+
+# FUNCTIONS
+
+
+
+# MAIN
+
+
+
